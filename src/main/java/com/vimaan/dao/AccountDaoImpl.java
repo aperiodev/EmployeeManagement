158bf819dd74ac88d9c4a5a38987be8279f206d9
balu <bala@apeiro.us>
2017-10-24 13:30:42 +0530

    balu

--- src/main/java/com/vimaan/dao/AccountDaoImpl.java
+++ src/main/java/com/vimaan/dao/AccountDaoImpl.java
@@ -1,31 +1,42 @@
-package com.vimaan.DAO;
+package com.vimaan.dao;
 
 import com.vimaan.model.Account;
 import com.vimaan.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
 import java.util.List;
-/**
- * Created by IT Division on 10-10-2017.
- */
+
 @Repository
 @Transactional
-public class AccountDaoImpl extends BaseDao implements AccountDao{
-
-    @Autowired
-    DataSource datasource;
+public class AccountDaoImpl extends BaseDao implements AccountDao {
 
     @Autowired
     SessionFactory sessionFactory;
 
     public Account getAccount(User user) {
-        String sql = "from Account where user_id='" + user.getId() + "'";
-        List<Account> accounts = sessionFactory.openSession().createQuery(sql).list();
+        String sql = "from Account where user.username='" + user.getUsername() + "'";
+        List<Account> accounts = getSession().createQuery(sql).list();
         return accounts.size() > 0 ? accounts.get(0) : null;
     }
 
+    public void registerAccount(Account account) {
+        try {
+            getSession().save(account);
+        } catch (Exception e) {
+            System.out.println(" exce   " + e.getMessage());
+        }
+
+    }
+
+    public void saveOrUpdateAccount(Account account) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.saveOrUpdate(account);
+        tx.commit();
+    }
 }
